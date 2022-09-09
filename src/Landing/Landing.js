b983a23c6d57d6d1b2accd2e95bb940da1944516
Laura Gonzalez <lauragonzalez@Lauras-MacBook-Pro-2.local>
2022-09-10 00:12:13 +0100

    Landing page added

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,9 +1,12 @@
+import './Landing.css';
 import Curtains from "../Curtains/Curtains";
+import MainButton from "../MainButton/MainButton.js"
 
 function Landing () {
     return <div>
         <Curtains></Curtains>
-        <h1>GALA SPIERDOLENIA 2022</h1>
+        <h1 className="main-header-landing">Gala Spierdolenia 2022</h1>
+        <MainButton buttonText="Zaczynajmy!"></MainButton>
     </div>
 }
 
---
+++ src/MainButton/MainButton.js
@@ -0,0 +1,6 @@
+import "./MainButton.css";
+
+function MainButton (props){
+return <button className="main-button">{props.buttonText}</button>
+}
+export default MainButton;
