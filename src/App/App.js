daa3fb7e543e355d043a347485b3ceaff352f502
Laura Gonzalez <lauragonzalez@Lauras-MacBook-Pro-2.local>
2022-09-09 20:30:15 +0100

    initial commit

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Landing from '../Landing/Landing.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Landing></Landing>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Landing/Landing.js
@@ -0,0 +1,10 @@
+import Curtains from "../Curtains/Curtains";
+
+function Landing () {
+    return <div>
+        <Curtains></Curtains>
+        <h1>GALA SPIERDOLENIA 2022</h1>
+    </div>
+}
+
+export default Landing;
