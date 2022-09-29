c99154cafee8607ef1b89da3c741f350e85617aa
MightyKielsa <104023970+MightyKielsa@users.noreply.github.com>
2022-09-29 13:31:28 +0100

    working on MemeCArd

--- src/Components/MemeCard/MemeCard.js
+++ src/Components/MemeCard/MemeCard.js
@@ -1,7 +1,27 @@
-function MemeCard() {
+function MemeCard(props) {
+  let emojiRep;
+  switch (props.emojiName) {
+    case "Love":
+      emojiRep = "x";
+      break;
+    case "Love":
+      emojiRep = "x";
+      break;
+  }
+
   return (
     <div className="meme-card">
-      <img src={props.memeSrc}></img>
+      <img src={props.memeSrc} alt="meme" className="img-meme-card"></img>
+      <div>
+        {props.emojis.map(function (item, index) {
+          return (
+            <div>
+              <img src=""></img>
+              <p>item.emojiClicks</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
